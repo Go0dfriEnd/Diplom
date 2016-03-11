d65e0663ea9eb58bda83f6086c58d22b3426abc3
sorokin <goodfriend3@gmail.ru>
2016-03-11 17:25:37 +0700

    Completed the direction of the wind

--- run.py
+++ run.py
@@ -8,35 +8,40 @@ from datetime import datetime, date
 # id городов с названиями и координатами тут: http://bulk.openweathermap.org/sample/city.list.json.gz
 # - Это обычный .json  в архиве
 
-city_id = '473537'
+city_id = '1496990'
 
-# TODO:(Сделаль) если отправить несуществующий id, то вернется json  с ошибкой. Доделать обарботку таких ошибок.
-params = {'id':city_id , 'APPID': settings.APPID}
-
+# TODO: если отправить несуществующий id, то вернется json  с ошибкой. Доделать обарботку таких ошибок.
+params = {'id': city_id, 'APPID': settings.APPID}
 response = requests.get(settings.url, params=params)
 
 print('full_result = ', response.json())
 print('all_keys = ', response.json().keys())
 print('Страна = ', response.json()['sys']['country'])
 print('Город = ', response.json()['name'])
-print('Уровень моря = ', response.json()['main']['sea_level'],'Метров над уровнем моря')
-print('Скорость ветра = ', response.json()['wind']['speed'],'Метров в секунду')
-a=response.json()['wind']['deg']
-if 89>a<=0 :
-    print('Напрваление', a,'на север')
-if 179>a<90:
-    print('Напрваление', a,'на запад')
-if 270>a<180:
-    print('Напрваление', a,'на юг')
-if 359>a<271:
-    print('Напрваление', a,'на восток')
-if 360<a:
-    print('Напрваление', a,'на север')
+print('Скорость ветра = ', response.json()['wind']['speed'], 'Метров в секунду')
+deg = response.json()['wind']['deg']
+print('напр ветра --> ', deg)
+if 30 > deg >= 0:
+    print('Направление', 'на Восток')
+if 60 > deg > 31:
+    print('Направление', 'на Северо-восток')
+if 119 > deg > 61:
+    print('Направление', 'на Север')
+if 149 > deg > 120:
+    print('Направление  на Северо-запад')
+if 209 > deg > 150:
+    print('Направление на Запад')
+if 239 > deg > 210:
+    print('Направление на Юго-запад')
+if 299 > deg > 240:
+    print('Направление на Юг')
+if 329 > deg > 300:
+    print('Направление на Юго-восток')
+if 360 > deg > 330:
+    print('Направление на Восток')
 
-print('Температура = ', response.json()['main']['temp_max']-274,'°C')
+print('Температура = ', response.json()['main']['temp_max'] - 274, '°C')
 date_mc = response.json()['dt']  # Дата в микросекундах
 print('Дата и время последнего запроса = ', datetime.fromtimestamp(date_mc))
 # Подробнее про HTTP запросы тут: http://ruseller.com/lessons.php?rub=28&id=1726
-# request - запрос, response - ответ
-
-
+#  request - запрос ,response - ответ
